3b8de07edca21f4a34374b07e3ebc19874c4ddf7
Iqbol-Abduqodirov <abdukadirovikbol@gmail.com>
2024-07-12 16:46:22 +0500

    Add files via upload

---
+++ trans1.py
@@ -0,0 +1,38 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+from wiki import wiki_bot
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = '7036154574:AAETuDKlNbuYG7BrnuqGl1xA0A7WOmLxS1c'
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    
+    await dp.start_polling(bot)
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
+    
